b7599a91c2132ebf9f7f9da22b9a25ad26919f2c
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-26 20:58:42 +0000

    Added Cilindro program and asked for user Input

---
+++ Cilindro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Insira a altura do cilindro");
+            string cilinderHeight = Console.ReadLine();
+
+            System.Console.WriteLine("Insira o raio do cilindro");
+            string cilinderRadius = Console.ReadLine();
+        }
+    }
+}
