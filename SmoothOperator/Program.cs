0726f16ebe80de500a824fde844084de524e68be
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-26 20:15:34 +0000

    Added SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string insNumb;
+
+            System.Console.WriteLine("Insere número inteiro: ");
+            insNumb = Console.ReadLine();
+
+            sbyte numbConv = Convert.ToSByte(insNumb);
+
+
+            numbConv--;
+            System.Console.WriteLine(numbConv);
+            numbConv++;
+            System.Console.WriteLine(numbConv);
+
+
+
+
+        }
+    }
+}
