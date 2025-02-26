634d4b44b4d70dc2c19edbebf568bd58af3764ce
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-26 20:52:39 +0000

    Added VariousOperators

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Insere número inteiro-não negativo:");
+            string userInput;
+
+            userInput = Console.ReadLine();
+
+            byte numbInputByte = Convert.ToByte(userInput);
+
+            System.Console.WriteLine(numbInputByte/2);
+            System.Console.WriteLine(numbInputByte << 3);
+            System.Console.WriteLine(numbInputByte ^ 6);
+            System.Console.WriteLine((numbInputByte > 10)? true:false);
+
+        }
+    }
+}
