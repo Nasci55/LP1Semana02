8af6159e23bda57785dfb83f2371260a175a8710
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-27 00:27:45 +0000

    Added 4 questions

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,17 +6,42 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            string userInput;
+            string userInput, response;
+
 
             while (true)
             {
+                System.Console.WriteLine("Ask me anything: ");
                 userInput = Console.ReadLine();
 
+                switch (userInput)
+                {
 
+                    case "How old are you":
+                        response = "I'm 100 years old!";
+                        System.Console.WriteLine(response);
+                        return;
+                    case "Who's your father?":
+                        response = "Alan Turing?";
+                        System.Console.WriteLine(response);
+                        return;
+                    case "Am I Alive?":
+                        response = "Right now yes but not for long.";
+                        System.Console.WriteLine(response);
+                        return;
+                    case "How can I become rich?":
+                        response = "Work hard";
+                        System.Console.WriteLine(response);
+                        return;
+
+
+                }
                 if (userInput == "Exit")
                 {
                     break;
                 }
+
+
 
 
 
