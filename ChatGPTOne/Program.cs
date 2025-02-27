ed8e361a99f6806278408f2559c3d7acd78bcee1
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-27 00:20:13 +0000

    Added ChatGPTOne and an exit option

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string userInput;
+
+            while (true)
+            {
+                userInput = Console.ReadLine();
+
+
+                if (userInput == "Exit")
+                {
+                    break;
+                }
+
+
+
+            }
+
+
+        }
+    }
+}
