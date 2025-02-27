c48efe49dd63b6921541a280da8aae3e6e182748
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-27 00:40:15 +0000

    Finished the AI

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -20,34 +20,30 @@ namespace ChatGPTOne
                     case "How old are you":
                         response = "I'm 100 years old!";
                         System.Console.WriteLine(response);
-                        return;
+                        break;
                     case "Who's your father?":
                         response = "Alan Turing?";
                         System.Console.WriteLine(response);
-                        return;
-                    case "Am I Alive?":
+                        break;
+                    case "Am I alive?":
                         response = "Right now yes but not for long.";
                         System.Console.WriteLine(response);
-                        return;
+                        break;
                     case "How can I become rich?":
                         response = "Work hard";
                         System.Console.WriteLine(response);
+                        break;
+                    case "EXIT":
                         return;
-
+                    default:
+                        response = "I know everything, less that one question";
+                        System.Console.WriteLine(response);
+                        break;
 
                 }
-                if (userInput == "Exit")
-                {
-                    break;
-                }
-
-
-
 
 
             }
-
-
         }
     }
 }
