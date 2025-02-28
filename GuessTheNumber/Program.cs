ac2d8d6022d63c4b1734c478cfe6ad990af095f1
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-28 17:35:23 +0000

    Finished the program

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,37 +10,38 @@ namespace GuessTheNumber
             Random rnd = new Random();
             int numberToGuess = rnd.Next(31);
 
-
+            int tries = 0;
 
 
             while (true)
             {
 
 
-                System.Console.WriteLine("Inser number:");
+                System.Console.Write("Insert number: ");
                 string numbInput = Console.ReadLine();
-
+                tries++;
 
                 int numb = Convert.ToInt16(numbInput);
 
-                if (numb < 0 || numb > 30)
+                if ((numb < 0) || (numb > 30))
                 {
                     System.Console.WriteLine("Numbers must be between 0 and 30. Try again.");
                 }
 
 
-                if (numb > numberToGuess)
+                else if (numb > numberToGuess)
                 {
-                    System.Console.WriteLine($"The hidden number is lower than{numb}. Try again");
+                    System.Console.WriteLine($"The hidden number is lower than {numb}. Try again.");
                 }
                 else if (numb < numberToGuess)
                 {
 
-                    System.Console.WriteLine($"The hidden number is higher than{numb}. Try again");
+                    System.Console.WriteLine($"The hidden number is higher than {numb}. Try again.");
                 }
                 else
                 {
-                    System.Console.WriteLine($"You found the hidden number {numberToGuess} after ");
+                    System.Console.WriteLine($"You found the hidden number {numberToGuess} after {tries} tries.");
+                    break;
                 }
 
             }
