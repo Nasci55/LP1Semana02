dd8cc689cb6ce8d386664fc0f5bf369d07cdc855
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-28 17:11:39 +0000

    Asked player for input

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int numberToGuess = rnd.Next(31);
+
+
+            while (true)
+            {
+
+
+                System.Console.WriteLine("Input a number from 0 to 30");
+                string numbInput = Console.ReadLine();
+
+                int numb = Convert.ToInt16(numbInput);
+
+                if (numb > numberToGuess)
+                {
+
+                }
+
+
+            }
+
+        }
+    }
+}
