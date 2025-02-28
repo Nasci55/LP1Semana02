c23d6cf5fa0eb040c5826a5974630b76c3c53114
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-28 17:21:28 +0000

    added help for the player to guess the number

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -11,20 +11,37 @@ namespace GuessTheNumber
             int numberToGuess = rnd.Next(31);
 
 
+
+
             while (true)
             {
 
 
-                System.Console.WriteLine("Input a number from 0 to 30");
+                System.Console.WriteLine("Inser number:");
                 string numbInput = Console.ReadLine();
+
 
                 int numb = Convert.ToInt16(numbInput);
 
-                if (numb > numberToGuess)
+                if (numb < 0 || numb > 30)
                 {
-
+                    System.Console.WriteLine("Numbers must be between 0 and 30. Try again.");
                 }
 
+
+                if (numb > numberToGuess)
+                {
+                    System.Console.WriteLine($"The hidden number is lower than{numb}. Try again");
+                }
+                else if (numb < numberToGuess)
+                {
+
+                    System.Console.WriteLine($"The hidden number is higher than{numb}. Try again");
+                }
+                else
+                {
+                    System.Console.WriteLine($"You found the hidden number {numberToGuess} after ");
+                }
 
             }
 
